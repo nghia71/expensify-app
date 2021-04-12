0d132fffb7252bd26ab1792fc60afe8a0f829c68
Nghia Doan <nghia71@gmail.com>
2021-04-12 15:47:30 -0400

    Extracted CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,55 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  // entry: './src/playground/redux-101.js',
-  // entry: './src/playground/redux-expensify.js',
-  // entry: './src/playground/hoc.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader', 
-      options: {
-        presets: [
-          ['@babel/preset-env', { targets: "defaults" }],
-          ['@babel/preset-react', { targets: "defaults" }],
-        ],
-        plugins: [
-          ["@babel/plugin-proposal-class-properties", { "loose": true}],
-        ]
-      },
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' }); 
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader', 
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }],
+            ['@babel/preset-react', { targets: "defaults" }],
+          ],
+          plugins: [
+            ["@babel/plugin-proposal-class-properties", { "loose": true}],
+          ]
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader , 
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }]
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
