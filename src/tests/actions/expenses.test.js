9a5b3ed16bdfe6c4cb81845084d47ee251c66581
Nghia Doan <nghia71@gmail.com>
2021-04-13 17:46:08 -0400

    Edit Expense added.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import {
     startAddExpense,
+    startEditExpense,
     startSetExpenses,
     startRemoveExpense,
     addExpense,
@@ -38,7 +39,7 @@ test('should remove expense from firebase', (done) => {
         expect(actions[0]).toEqual({
             type: 'REMOVE_EXPENSE',
             id
-        })
+        });
         return database.ref(`expenses/${id}`).once('value');
     }).then((snapshot) => {
         expect(snapshot.val()).toBeFalsy();
@@ -54,7 +55,25 @@ test('should setup edit expense action object', () => {
         updates: {
             notes: 'New note value'
         }
-    })
+    });
+});
+
+test('should edit expense from firebase', (done) => {
+    const store = createMockStore({});
+    const id = expenses[0].id;
+    const updates = { amount: 21045 };
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
+    });    
 });
 
 test('should setup add expense action object with provided value', () => {
